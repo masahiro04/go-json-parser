8e7b0334c63a2af27fbda2037b21a23f476fdad1
masahirookubo <stmhamachiii@gmail.com>
2021-12-06 01:08:21 +0900

    lexerの追加

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+import "fmt"
+
+type Token struct {
+	Type
+	Lit
+}
+
+type Type string
+
+type Lit []rune
+
+// Types
+const (
+	INVALID  = "INVALID"
+	EOF      = "EOF"
+	COMMA    = "COMMA"
+	COLON    = "COLON"
+	LBRACE   = "LBRACE"
+	RBRACE   = "RBRACE"
+	LBRACKET = "LBRACKET"
+	RBRACKET = "RBRACKET"
+	STRING   = "STRING"
+	INTEGER  = "INTEGER"
+)
+
+func NewToken(typ Type, lit string) Token {
+	return Token{Type: typ, Lit: []rune(lit)}
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("{token.%s, '%s'},", t.Type, string(t.Lit))
+}
