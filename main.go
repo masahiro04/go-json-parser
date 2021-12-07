48d812bd2d3ad5f2ccf2a958098f842ff707ce4f
masahirookubo <stmhamachiii@gmail.com>
2021-12-07 23:25:46 +0900

    parserの実行関数の追加

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"go-json-parser/lexer"
+	"go-json-parser/parser"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("test")
+	if len(os.Args) < 2 {
+		panic("no valid file name or path provided for file!")
+	}
+
+	path := os.Args[1]
+	absPath, _ := filepath.Abs(path)
+	data, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	l := lexer.NewLexer(data)
+	p := parser.NewParser(l)
+
+	ast := p.Parse()
+
+	js, _ := json.MarshalIndent(ast, "", "    ")
+	fmt.Println(string(js))
 }
+
+// ./sample.jsonを読み込ませる
